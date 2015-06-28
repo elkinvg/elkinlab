f567ebb331931986a7422a6b9383064ecbfccb3b
V.G.Elkin <elkinvg@gmail.com>
2015-06-28 15:42:32 +0300

    2

--- php/cnt.php
+++ php/cnt.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
 $fileLocation = "./hlpme/cnt.txt";
-=======
-$fileLocation = getenv("DOCUMENT_ROOT") . ".labs/hlpme/cnt.txt";
->>>>>>> fa367920279e693a64bb2253546f1a3a965dc00a
     $file = fopen( $fileLocation, "a+" );
     
     $info = $context['user'] . " " . date("Y-m-d H:i:s")  ."\n";
